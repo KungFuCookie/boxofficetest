59106136a19dd146382c57a14d952d76a0c13657
HQ\steve.cooke <steve.cooke@webedia.co.uk>
2019-10-01 13:43:53 +0100

    added shared components

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -2,10 +2,17 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Row, Col } from 'react-bootstrap';
 
+import Poster from '@boxoffice/shared-components/src/Poster';
+
 class Home extends Component {
   render() {
     return (
       <div className='home'>
+        <Row>
+          <Col>
+            <Poster src='https://cdn.collider.com/wp-content/uploads/2019/03/avengers-endgame-poster-768x1137.jpg' />
+          </Col>
+        </Row>
         <Row>
           <Col>
             <p>Hello from Home</p>
